03802f24c9a70bf77d95fe445d761674cbf4f353
FridCodes <new.frid.1.0@gmail.com>
2021-07-23 20:57:07 +0000

    updated code to print Jenkins URL and login credentials

--- simpleCode.py
+++ simpleCode.py
@@ -8,6 +8,16 @@ if __name__ == '__main__':
 
 print("My name is Fridaws and I love to code.")
 
+#This is the URL to my Jenkins server.
+
+print("Jenkins server URL: http://40.113.241.204:8080/login?from=%2F")
+
+#This is the username and  password to access my Jenkins server.
+
+print("Admin")
+
+print("56651edcd3e7440d82984a73999a383c")
+
 
 
 
